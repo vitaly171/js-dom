a028cd61c5b676ef271395a8e3c14cbd5debc68c
vitaly171 <72026466+vitaly171@users.noreply.github.com>
2022-11-09 00:21:00 +0300

    task 3

---
+++ js/task-03.js
@@ -0,0 +1,28 @@
+// Use an array of images objects to create nested img tags in li.
+// Use template strings and insertAdjacentHTML() to create markup.
+
+const images = [
+  {
+    url: 'https://images.pexels.com/photos/140134/pexels-photo-140134.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260',
+    alt: 'White and Black Long Fur Cat',
+  },
+  {
+    url: 'https://images.pexels.com/photos/213399/pexels-photo-213399.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260',
+    alt: 'Orange and White Koi Fish Near Yellow Koi Fish',
+  },
+  {
+    url: 'https://images.pexels.com/photos/219943/pexels-photo-219943.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260',
+    alt: 'Group of Horses Running',
+  },
+];
+
+const galleryEl = document.querySelector('#gallery');
+
+const markup = images
+  .map(el => {
+    console.log(el.alt);
+    return `<li> <img src=${el.url} alt= ${el.alt}></li>`;
+  })
+  .join('');
+
+galleryEl.insertAdjacentHTML('afterbegin', markup);
