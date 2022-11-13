f1ff3ab536bc1f470c3c84256faa23fc9693a3bb
vitaly171 <72026466+vitaly171@users.noreply.github.com>
2022-11-13 22:18:59 +0300

    5 exercise start

---
+++ js/task-05.js
@@ -0,0 +1,9 @@
+// Напиши скрипт который, при наборе текста в инпуте
+// input#name - input(событие input), подставляет его текущее значение
+// в span#name - output.Если инпут пустой, в спане должна отображаться строка 'незнакомец'.
+
+const returnLink = document.querySelector('a');
+const p = document.querySelector('p');
+
+returnLink.classList.add('return');
+p.classList.add('p');
