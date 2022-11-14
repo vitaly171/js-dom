80619f13418c6cfae91af965f161c5b414642cfb
vitaly171 <72026466+vitaly171@users.noreply.github.com>
2022-11-15 00:09:36 +0300

    task-5 Done

--- js/task-05.js
+++ js/task-05.js
@@ -1,9 +1,31 @@
-// Напиши скрипт который, при наборе текста в инпуте
-// input#name - input(событие input), подставляет его текущее значение
-// в span#name - output.Если инпут пустой, в спане должна отображаться строка 'незнакомец'.
+// Write a script that, when typing in the input
+// input#name - input(input event), substitutes its current value
+// in span#name - output. If the input is empty,
+// the span should display the string 'stranger'.
 
 const returnLink = document.querySelector('a');
 const p = document.querySelector('p');
 
 returnLink.classList.add('return');
 p.classList.add('p');
+
+const input = document.querySelector('#name-input');
+let outputName = document.querySelector('#name-output');
+const btnClean = document.querySelector('#clean-input');
+console.log(btnClean);
+input.addEventListener('input', ewt => {
+  if (input.value === '') {
+    outputName.textContent = 'stranger';
+  } else {
+    outputName.textContent = ewt.target.value;
+  }
+});
+
+function clean() {
+  input.value = '';
+  outputName.textContent = 'stranger';
+}
+
+btnClean.addEventListener('click', () => {
+  clean();
+});
