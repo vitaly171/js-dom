976c80d00239da64e09b97290ee3387f9de2cb60
vitaly171 <72026466+vitaly171@users.noreply.github.com>
2022-11-15 22:02:18 +0300

    task-6 done

--- js/task-06.js
+++ js/task-06.js
@@ -1,1 +1,26 @@
+// Write a script that, when focus is lost on an input,
+// checks its contents for the correct number of characters.
+const inputRef = document.querySelector('#validation-input');
+const returnLink = document.querySelector('a');
+const p = document.querySelector('p');
 
+inputRef.classList.add('validation-input');
+
+const validation = () => {
+  if (inputRef.value.length === 6) {
+    inputRef.classList.remove('invalid');
+    return inputRef.classList.add('valid');
+  }
+  if (inputRef.value.length === 0) {
+    inputRef.classList.remove('valid');
+    inputRef.classList.remove('invalid');
+  } else {
+    inputRef.classList.add('invalid');
+    inputRef.classList.remove('valid');
+  }
+};
+
+inputRef.addEventListener('blur', validation);
+
+returnLink.classList.add('return');
+p.classList.add('p');
